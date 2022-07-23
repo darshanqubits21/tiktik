fdfd47b78b7fc6964846a19cb3d1b2caf3b8090e
darshanqubits21 <darshanqubits@gmail.com>
2022-07-23 16:13:34 +0545

    implement google auth in project

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Logo from "/utils/tiktik-logo.png";
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="w-full flex justify-between item-center border-b-2 border-gray-200 py-2 px-4">
+    <div className="flex justify-between w-full px-4 py-2 border-b-2 border-gray-200 item-center">
       <Link href="/">
         <div className="w-[100px] md:[130px]">
           <Image
@@ -20,6 +22,21 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <div>
+            <GoogleLogin
+              onSuccess={(response) => {
+                console.log(response);
+              }}
+              onError={() => console.log("Error")}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
